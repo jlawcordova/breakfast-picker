3815624c4250eb0225a81b10bb22ad469e7c7685
J. Law. Cordova <jlawrence.cordova@netzontech.com>
2023-10-27 11:43:42 +0800

    chore: add proper margins

--- src/components/CanvasItem.js
+++ src/components/CanvasItem.js
@@ -1,8 +1,8 @@
 export function CanvasItem({name, image}) {
   return (
-    <div className="cursor-pointer rounded-md shadow-md p-8 py-2 flex items-center w-96">
+    <div className="cursor-pointer rounded-md shadow-md p-8 py-2 flex items-center w-64">
       <img className="w-24 h-24 object-contain" src={image} alt={name} />
-      <h2>{name}</h2>
+      <h2 className="ml-8">{name}</h2>
     </div>
   );
 }
