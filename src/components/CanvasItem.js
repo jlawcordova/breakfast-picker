5e8bb9a3dadd550d64ca9bcec26877e50ed31338
J. Law. Cordova <jlawrence.cordova@netzontech.com>
2023-10-27 11:36:49 +0800

    chore: add canvas item params

--- src/components/CanvasItem.js
+++ src/components/CanvasItem.js
@@ -1,12 +1,8 @@
-export function CanvasItem() {
+export function CanvasItem({name, image}) {
   return (
     <div className="cursor-pointer w-auto rounded-md shadow-md p-8 py-2 flex items-center">
-      <img
-        className=" w-24 h-24 object-contain"
-        src={"/coffee.png"}
-        alt={"Sample Item"}
-      />
-      <h2>{"Sample Item"}</h2>
+      <img className="w-24 h-24 object-contain" src={image} alt={name} />
+      <h2>{name}</h2>
     </div>
   );
 }
